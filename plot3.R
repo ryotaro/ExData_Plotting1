659b7816e6f92d71e3723ec7fe5647ee7893856d
Ryotaro Ikeda <ryo@longsleeper.com>
2017-03-23 23:10:01 +0900

    fix bug

--- plot3.R
+++ plot3.R
@@ -9,33 +9,7 @@ png(filename = './plot3.png',
     width = 480, 
     height = 480)
 
-par(mfrow = c(2, 2))
-
-# Top left
-plot(df$timestamp,
-     df$Global_active_power, 
-     type = 'n',
-     xlab = '',
-     ylab = 'Energy sub metering (killowatts)',
-     ylim = range(1:8))
-lines(
-  df$timestamp,
-  df$Global_active_power,
-  col = 'black')
-
-# Top right
-plot(df$timestamp,
-     df$Voltage,
-     type = 'n',
-     xlab = 'datetime',
-     ylab = 'Voltage',
-     ylim = range(230:250))
-lines(
-  df$timestamp, 
-  df$Voltage, 
-  col = 'black')
-
-# Bottom left
+par(mfrow = c(1, 1))
 plot(df$timestamp,
      df$Sub_metering_1,
      type = 'n',
@@ -59,17 +33,6 @@ legend('topright',
        lty=1,
        lwd=1,
        col=c('black', 'red', 'blue'), 
-       bty='n')
-
-# Bottom right
-plot(df$timestamp,
-     df$Global_reactive_power,
-     type = 'n',
-     xlab = 'datetime',
-     ylab = 'Global_reactive_power',
-     ylim = c(0, 0.5))
-lines(df$timestamp,
-      df$Global_reactive_power,
-      col = 'black')
+       bty='o')
 
 dev.off()
